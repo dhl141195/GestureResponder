5121f733ef044b6a8a0eef0f0022a2680dcac491
DHL <dhl1402@gmail.com>
2017-09-04 16:56:29 +0700

    simple move view

--- src/App.js
+++ src/App.js
@@ -9,6 +9,39 @@ import {
 StatusBar.setHidden(true);
 
 export default class App extends Component {
+
+    state = {
+        xRoot: 0,
+        yRoot: 0,
+        top: 0,
+        left: 0
+    }
+
+    onTouchHandler = (event) => {
+        const { pageX, pageY } = event.nativeEvent;
+
+        this.setState(prevState => {
+            return {
+                // Trừ đi prevState.left và prevState.top để mỗi khi click bắt đầu drag sẽ set xRoot, yRoot bằng vị trí ban đầu
+                // Và sau đó trong onMoveHandler sẽ tính ra left và top để duy trì vị trí của lần drag & drop trước
+                // Cũng có thể check nếu xRoot và yRoot khác 0 thì mới cập nhật state mỗi khi click nhưng nếu làm như vậy
+                // sẽ bị lệch 1 chút khi click bắt đầu drag vì điểm click ban đầu k được set lại, các lần click sau sẽ
+                // đc tính dựa vào điểm click đầu tiên
+                xRoot: pageX - prevState.left,
+                yRoot: pageY - prevState.top
+            }
+        })
+    }
+
+    onMoveHandler = (event) => {
+        const { pageX, pageY } = event.nativeEvent;
+
+        this.setState(prevState => ({
+            left: pageX - prevState.xRoot,
+            top: pageY - prevState.yRoot,
+        }));
+    }
+
     render() {
 
         const {
@@ -17,9 +50,23 @@ export default class App extends Component {
             text
         } = styles;
 
+        const { top, left } = this.state;
+
         return (
             <View style={container}>
-                <View style={textWrapper}>
+                <View
+                    onStartShouldSetResponder={() => true}
+                    onMoveShouldSetResponder={() => true}
+                    onResponderGrant={this.onTouchHandler}
+                    onResponderMove={this.onMoveHandler}
+                    style={[
+                        textWrapper,
+                        {
+                            top,
+                            left
+                        }
+                    ]}
+                >
                     <Text style={text}>
                         Hello, I'm DHL
                     </Text>
